d05c8fbf243bb90e38bf6b84552e1596ef8424ee
azimy-hamid <hamidazimy875@gmail.com>
2024-10-22 23:08:00 +0800

    Started working on admin dashboard

--- frontend/src/services/adminEndpoints.js
+++ frontend/src/services/adminEndpoints.js
@@ -4,8 +4,11 @@ const API_URL = `${import.meta.env.VITE_API_BASE_URL}/admin`;
 
 const createAdmin = async (userData) => {
   try {
+    const token = localStorage.getItem("token");
+
     const response = await axios.post(`${API_URL}/create-admin`, userData, {
       headers: { "Content-Type": "application/json" },
+      Authorization: `Bearer ${token}`,
     });
 
     const data = response.data;
