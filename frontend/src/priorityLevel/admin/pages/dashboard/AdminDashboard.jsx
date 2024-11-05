425417a49e4165ad723f0a7f2841bf31a88f0942
azimy-hamid <hamidazimy875@gmail.com>
2024-11-05 17:31:08 +0800

    Added a chart

--- frontend/src/priorityLevel/admin/pages/dashboard/AdminDashboard.jsx
+++ frontend/src/priorityLevel/admin/pages/dashboard/AdminDashboard.jsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 
 import { alpha } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import AppNavbar from "./components/AppNavbar.jsx";
 import Header from "./components/Header.jsx";
 // import MainGrid from "./components/MainGrid.jsx";
 import SideMenu from "./components/SideMenu.jsx";
-import AppTheme from "../../../../theme/AppTheme.jsx";
 import {
   chartsCustomizations,
   dataGridCustomizations,
   datePickersCustomizations,
   treeViewCustomizations,
 } from "../../../../theme/customizations/index.js";
+import Grid from "@mui/material/Grid"; // Import Grid component
+import { NumberOfStudentsChart } from "./components/NumberOfStudentsChart.jsx";
 
 const xThemeComponents = {
   ...chartsCustomizations,
@@ -25,36 +25,38 @@ const xThemeComponents = {
 
 export default function AdminDashboard(props) {
   return (
-    <AppTheme {...props} themeComponents={xThemeComponents}>
-      <CssBaseline enableColorScheme />
-      <Box sx={{ display: "flex" }}>
-        <SideMenu />
-        <AppNavbar />
-        {/* Main content */}
-        <Box
-          component="main"
-          sx={(theme) => ({
-            flexGrow: 1,
-            backgroundColor: theme.vars
-              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-              : alpha(theme.palette.background.default, 1),
-            overflow: "auto",
-          })}
+    <Box sx={{ display: "flex" }}>
+      <SideMenu />
+      <AppNavbar />
+      {/* Main content */}
+      <Box
+        component="main"
+        sx={(theme) => ({
+          flexGrow: 1,
+          backgroundColor: theme.vars
+            ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+            : alpha(theme.palette.background.default, 1),
+          overflow: "auto",
+        })}
+      >
+        <Stack
+          spacing={2}
+          sx={{
+            alignItems: "center",
+            mx: 3,
+            pb: 5,
+            mt: { xs: 8, md: 0 },
+          }}
         >
-          <Stack
-            spacing={2}
-            sx={{
-              alignItems: "center",
-              mx: 3,
-              pb: 5,
-              mt: { xs: 8, md: 0 },
-            }}
-          >
-            <Header />
-            {/* <MainGrid /> */}
-          </Stack>
-        </Box>
+          <Header />
+          {/* <MainGrid /> */}
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              {<NumberOfStudentsChart />}
+            </Grid>
+          </Grid>
+        </Stack>
       </Box>
-    </AppTheme>
+    </Box>
   );
 }
