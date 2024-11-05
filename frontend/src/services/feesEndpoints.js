9ad17e6e6e29438de478b2e5a414f9e7ab4fe7a0
azimy-hamid <hamidazimy875@gmail.com>
2024-11-05 17:40:30 +0800

    Created services to call the api

---
+++ frontend/src/services/feesEndpoints.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/fees`;
+
+export const payFee = async (feeData) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await axios.post(`${API_URL}/create-fee`, feeData, {
+      headers: { "Content-Type": "application/json" },
+      Authorization: `Bearer ${token}`,
+    });
+
+    const data = response.data;
+
+    return data;
+  } catch (error) {
+    console.error("Error paying student fee:", error);
+    throw error; // Propagate the error to handle it where the function is called
+  }
+};
+
+export const updateFee = async (feeData, feeId) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await axios.put(
+      `${API_URL}/update-student-details-by-id/${feeId}`,
+      feeData,
+      {
+        headers: { "Content-Type": "application/json" },
+        Authorization: `Bearer ${token}`,
+      }
+    );
+
+    const data = response.data;
+
+    return data;
+  } catch (error) {
+    console.error("Error updating student fee:", error);
+    throw error; // Propagate the error to handle it where the function is called
+  }
+};
+
+export const getAllFees = async () => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await axios.get(`${API_URL}/get-all-fees`, {
+      headers: { "Content-Type": "application/json" },
+      Authorization: `Bearer ${token}`,
+    });
+
+    const data = response.data;
+
+    return data;
+  } catch (error) {
+    console.error("Error getting all fees:", error);
+    throw error;
+  }
+};
---
+++ frontend/src/services/studentSubjectEndpoints.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/student-subject`;
+
+export const assignSubject = async (studentSubjectData) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await axios.post(
+      `${API_URL}/create-student-subject`,
+      studentSubjectData,
+      {
+        headers: { "Content-Type": "application/json" },
+        Authorization: `Bearer ${token}`,
+      }
+    );
+
+    const data = response.data;
+
+    return data;
+  } catch (error) {
+    console.error("Error assigning subject:", error);
+    throw error; // Propagate the error to handle it where the function is called
+  }
+};
+
+// export const updateStudent = async (studentData, studentId) => {
+//   try {
+//     const token = localStorage.getItem("token");
+
+//     const response = await axios.put(
+//       `${API_URL}/update-student-details-by-id/${studentId}`,
+//       studentData,
+//       {
+//         headers: { "Content-Type": "application/json" },
+//         Authorization: `Bearer ${token}`,
+//       }
+//     );
+
+//     const data = response.data;
+
+//     return data;
+//   } catch (error) {
+//     console.error("Error Creating Student:", error);
+//     throw error; // Propagate the error to handle it where the function is called
+//   }
+// };
