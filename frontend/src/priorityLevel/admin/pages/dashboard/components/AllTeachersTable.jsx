cad8e2e2a7247576462aab1a1b48d2122c4f0363
azimy-hamid <hamidazimy875@gmail.com>
2024-11-13 16:12:25 +0800

    Modified the frontend to be able to delete a teacher

--- frontend/src/priorityLevel/admin/pages/dashboard/components/AllTeachersTable.jsx
+++ frontend/src/priorityLevel/admin/pages/dashboard/components/AllTeachersTable.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Snackbar, Alert } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { getAllTeachers } from "../../../../../services/teacherEndpoints.js";
 import { useNavigate } from "react-router-dom";
+import { deleteTeacher } from "../../../../../services/teacherEndpoints.js";
 
 export default function AllTeachersTable() {
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarSeverity, setSnackbarSeverity] = useState("success");
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
   const [teachers, setTeachers] = useState([]);
   const navigate = useNavigate();
+
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
 
   useEffect(() => {
     getAllTeachers()
@@ -22,8 +31,36 @@ export default function AllTeachersTable() {
     navigate(`/admin/teacher-details/${teacherId}`);
   };
 
-  const handleUpdateClick = (teacherId) => {
-    navigate(`/admin/update-teacher/${teacherId}`); // Navigate to the update route
+  const handleDeleteClick = async (teacherId) => {
+    try {
+      const deletedTeacher = await deleteTeacher(teacherId);
+      console.log(deletedTeacher);
+
+      if (deletedSubject.deletedTeacherMessage) {
+        setSnackbarMessage(deletedSubject.deleteTeacherMessage);
+        setSnackbarSeverity(deletedSubject.deleteTeacherMessage);
+
+        // Remove the subject from the list after deletion
+        setSubjects((prevSubjects) =>
+          prevSubjects.filter((subject) => subject.subject_id_pk !== teacherId)
+        );
+      } else {
+        setSnackbarMessage(deletedSubject.deleteTeacherMessage);
+        setSnackbarSeverity("error");
+      }
+
+      // Open the snackbar
+      setSnackbarOpen(true);
+    } catch (error) {
+      const errorMessage =
+        error.response && error.response.data.deleteTeacherMessage
+          ? error.response.data.deleteTeacherMessage
+          : "Server error. Please try again later.";
+
+      setSnackbarMessage(errorMessage);
+      setSnackbarSeverity("error");
+      setSnackbarOpen(true);
+    }
   };
 
   const columns = [
@@ -31,7 +68,7 @@ export default function AllTeachersTable() {
     { field: "teacher_last_name", headerName: "Last Name", flex: 1 },
     { field: "email", headerName: "Email", flex: 1 },
     { field: "phone", headerName: "Phone", flex: 1 },
-    { field: "working_days", headerName: "Working Days", flex: 1 },
+    { field: "working_days", headerName: "Working Days", flex: 2 },
     {
       field: "details",
       headerName: "Details",
@@ -49,16 +86,16 @@ export default function AllTeachersTable() {
       filterable: false,
     },
     {
-      field: "update",
-      headerName: "Update",
-      flex: 1,
+      field: "delete",
+      headerName: "Delete",
+      flex: 1.5,
       renderCell: (params) => (
         <Button
           variant="outlined"
           color="primary"
-          onClick={() => handleUpdateClick(params.row.teacher_id_pk)}
+          onClick={() => handleDeleteClick(params.row.teacher_id_pk)}
         >
-          Update
+          Delete Teacher
         </Button>
       ),
       sortable: false,
@@ -88,6 +125,20 @@ export default function AllTeachersTable() {
         disableSelectionOnClick
         autoHeight={false}
       />
+
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity={snackbarSeverity}
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 }
